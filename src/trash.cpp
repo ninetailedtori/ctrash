68bc228e1cd44f4eaeb20c0b08b3a816bffe99f9
ninetailedtori <ninetailedtori@uwu.gal>
2025-11-14 03:54:26 +0000

    Biiiiiig push! clangd manages our clang-tidy formatting. trash object base has been written.
    Signed-off-by: ninetailedtori <ninetailedtori@uwu.gal>

--- src/trash.cpp
+++ src/trash.cpp
@@ -1,1 +1,70 @@
 #include "trash.hpp"
+
+#include <utility>
+
+ctrash::trash::trash(std::string name) : t_name(std::move(name)),
+                                         t_description(),
+                                         t_location() {}
+
+ctrash::trash::trash
+(const std::filesystem::path &location) : t_name(location.string()),
+                                          t_description(),
+                                          t_location(location) {}
+
+ctrash::trash::trash
+(
+    std::string name,
+    std::string description,
+    std::filesystem::path location
+) : t_name(std::move(name)),
+    t_description(std::move(description)),
+    t_location(std::move(location)) {}
+
+// Copy/Move Constructors
+ctrash::trash::trash(const trash &other) = default;
+
+ctrash::trash::trash(trash &&other) noexcept = default;
+
+// Destructor
+ctrash::trash::~trash() = default;
+
+// Getters/Setters
+auto ctrash::trash::name() -> std::string & { return t_name; }
+
+auto ctrash::trash::description() -> std::string & { return t_description; }
+
+auto ctrash::trash::location() -> std::filesystem::path & { return t_location; }
+
+auto ctrash::trash::name() const -> std::string { return t_name; }
+
+auto ctrash::trash::description() const -> std::string { return t_description; }
+
+auto ctrash::trash::location() const -> std::filesystem::path
+{
+    return t_location;
+}
+
+auto ctrash::trash::iterator() const -> std::filesystem::directory_iterator
+{
+    return std::filesystem::directory_iterator(t_location);
+}
+
+auto ctrash::trash::operator=(const trash &other) -> ctrash::trash & = default;
+
+auto ctrash::trash::operator=
+(trash &&other) noexcept -> ctrash::trash & = default;
+
+auto ctrash::trash::operator==
+(const trash &other) const -> bool { return t_location == other.t_location; }
+
+auto ctrash::trash::operator!=
+(const trash &other) const -> bool { return t_location != other.t_location; }
+
+auto ctrash::operator<<(std::ostream &os, const trash &trash) -> std::ostream &
+{
+    return os << trash.t_name
+           << "("
+           << trash.t_description
+           << "): \""
+           << trash.t_location.c_str() << "\"";
+}
--- src/trash.hpp
+++ src/trash.hpp
@@ -15,7 +15,7 @@
 namespace ctrash
 {
     /**
-     * @brief typedef for stat struct (<sys/stat.h>).
+     * @brief Holds file metadata.
      *
      * @param {dev_t}       st_dev      ID of device containing file
      * @param {ino_t}       st_ino      inode number
@@ -31,45 +31,158 @@ namespace ctrash
      * @param {time_t}      st_mtime    time of last modification
      * @param {time_t}      st_ctime    time of last status change
      */
-    using trash_object = struct stat;
+    using f_metadata = struct stat;
 
     /**
-     * @brief holds metadata, location, and specific trash locations.
-     *
+     * @brief Trash location class, with labelling.
      */
-    class trash_container
+    class trash
     {
     private:
-        std::string name;
-        std::string description;
+        /**
+         * @brief Name of trash object.
+         */
+        std::string t_name;
+        /**
+         * @brief Description of trash object.
+         */
+        std::string t_description;
 
-        std::filesystem::path location;
-        std::filesystem::directory_iterator dir_it;
+        /**
+         * @brief Path to trash location.
+         */
+        std::filesystem::path t_location;
 
     public:
         // Basic Constructors
-        trash_container();
 
-        trash_container
+        /**
+         * @brief Remove default constructor.
+         */
+        trash() = delete;
+
+        /**
+         * @brief Construct by name.
+         * @param name Name of trash object.
+         */
+        explicit trash(std::string name);
+
+        /**
+         * @brief Construct by location. Name will be assigned to path.
+         * @param location Path to trash location.
+         */
+        explicit trash(const std::filesystem::path &location);
+
+        /**
+         * @brief Initializer List Constructor
+         * @param name Name of trash object
+         * @param description Description of trash object
+         * @param location Path to trash location
+         */
+        trash
         (
             std::string name,
             std::string description,
             std::filesystem::path location
         );
 
-        explicit trash_container(std::filesystem::path location);
+        // Copy/Move Constructors
 
-        // Copy-Move Constructors
-        trash_container(const trash_container &other);
+        /**
+         * @brief Copy constructor
+         * @param other Trash object to copy
+         */
+        trash(const trash &other);
 
-        trash_container(trash_container &&other) noexcept;
+        /**
+         * @brief Move constructor
+         * @param other Trash object to move
+         */
+        trash(trash &&other) noexcept;
 
         // Destructor
-        ~trash_container();
-    };
 
-    /**
-     * @brief Static member functions to handle trash manipulation.
-     *
-     */
+        /**
+         * @brief Destructor.
+         */
+        ~trash();
+
+        // Getters/Setters
+
+        /**
+         * @brief Setter
+         * @return t_name
+         */
+        auto name() -> std::string &;
+
+        /**
+         * @brief Setter
+         * @return t_description
+         */
+        auto description() -> std::string &;
+
+        /**
+         * @brief Setter
+         * @return t_location
+         */
+        auto location() -> std::filesystem::path &;
+
+        /**
+         * @brief Getter
+         * @return t_name
+         */
+        [[nodiscard]] auto name() const -> std::string;
+
+        /**
+         * @brief Getter
+         * @return t_name
+         */
+        [[nodiscard]] auto description() const -> std::string;
+
+        /**
+         * @brief Getter
+         * @return t_name
+         */
+        [[nodiscard]] auto location() const -> std::filesystem::path;
+
+        /**
+         * @brief Getter
+         * @return t_name
+         */
+        [[nodiscard]] auto
+        iterator() const -> std::filesystem::directory_iterator;
+
+        // Operators
+
+        /**
+         * @brief Copy operator
+         * @param other Trash object to copy
+         * @return *this
+         */
+        auto operator=(const trash &other) -> trash &;
+
+        /**
+         * @brief Move operator
+         * @param other Trash object to move
+         * @return *this
+         */
+        auto operator=(trash &&other) noexcept -> trash &;
+
+        /**
+         * @brief Equality operator
+         * @param other Trash object to compare
+         * @return bool - true if locations are equal.
+         */
+        auto operator==(const trash &other) const -> bool;
+
+        /**
+         * @brief Inequality operator
+         * @param other Trash object to compare
+         * @return bool - true if locations are inequal.
+         */
+        auto operator!=(const trash &other) const -> bool;
+
+        friend auto operator<<
+        (std::ostream &os, const trash &trash) -> std::ostream &;
+    };
 }
