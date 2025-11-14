f74e4f363f8b4a38ca447b8cf1ad56bc1ff40c6b
ninetailedtori <ninetailedtori@uwu.gal>
2025-11-14 04:30:32 +0000

    Few new operators - incomplete. Added TODO note - how to handle trashed file objects. Handle as trash_file, use f_ as prefix.
    Signed-off-by: ninetailedtori <ninetailedtori@uwu.gal>

--- src/trash.hpp
+++ src/trash.hpp
@@ -14,8 +14,20 @@
 
 namespace ctrash
 {
+    #if defined _WIN32 || defined _WIN64 || defined __CYGWIN__
+    inline static const char *_os = "WIN";
+    #elif defined __APPLE__ || __MACH__
+    inline static const char *_os = "APPLE";
+    #elif defined __FreeBSD__
+    inline static const char *_os = "FreeBSD";
+    #elif defined __linux__
+    inline static const char *_os = "LINUX";
+    #elif defined unix || defined __unix || defined __unix__
+    inline static const char *_os = "UNIX";
+    #endif
+
     /**
-     * @brief Holds file metadata.
+     * @brief Holds file metadata
      *
      * @param {dev_t}       st_dev      ID of device containing file
      * @param {ino_t}       st_ino      inode number
@@ -34,22 +46,22 @@ namespace ctrash
     using f_metadata = struct stat;
 
     /**
-     * @brief Trash location class, with labelling.
+     * @brief Trash location class, with labelling and handling
      */
     class trash
     {
     private:
         /**
-         * @brief Name of trash object.
+         * @brief Name of trash object
          */
         std::string t_name;
         /**
-         * @brief Description of trash object.
+         * @brief Description of trash object
          */
         std::string t_description;
 
         /**
-         * @brief Path to trash location.
+         * @brief Path to trash location
          */
         std::filesystem::path t_location;
 
@@ -57,19 +69,21 @@ namespace ctrash
         // Basic Constructors
 
         /**
-         * @brief Remove default constructor.
+         * @brief Remove default constructor
          */
         trash() = delete;
 
         /**
-         * @brief Construct by name.
-         * @param name Name of trash object.
+         * @brief Name Constructor.
+         * @param name Name of trash object
          */
         explicit trash(std::string name);
 
         /**
-         * @brief Construct by location. Name will be assigned to path.
-         * @param location Path to trash location.
+         * @brief Location Constructor
+         *
+         * Name will be assigned to the trash path.
+         * @param location Path to trash location
          */
         explicit trash(const std::filesystem::path &location);
 
@@ -103,7 +117,7 @@ namespace ctrash
         // Destructor
 
         /**
-         * @brief Destructor.
+         * @brief Destructor
          */
         ~trash();
 
@@ -171,16 +185,38 @@ namespace ctrash
         /**
          * @brief Equality operator
          * @param other Trash object to compare
-         * @return bool - true if locations are equal.
+         * @return bool - true if locations are equal
          */
         auto operator==(const trash &other) const -> bool;
 
         /**
          * @brief Inequality operator
          * @param other Trash object to compare
-         * @return bool - true if locations are inequal.
+         * @return bool - true if locations are inequal
          */
         auto operator!=(const trash &other) const -> bool;
+
+        /* TODO: Note, this is not a good idea to do like this.
+         * We should be aiming to manage all trashed files as a new object class,
+         * and this will allow us to include better completions for this as well.
+         * It also lets us handle file metadata better than typedef-ing.
+         */
+
+        /**
+         * @brief Send file to trash
+         * @param f_delete Path of file to trash
+         * @return Path to file deleted.
+         */
+        auto operator+=
+        (std::filesystem::path f_delete) -> std::filesystem::path &;
+
+        /**
+         * @brief Restore file from trash
+         * @param f_restore Path of file to restore
+         * @return Path to file restored.
+         */
+        auto operator-=
+        (std::filesystem::path f_restore) -> std::filesystem::path &;
 
         friend auto operator<<
         (std::ostream &os, const trash &trash) -> std::ostream &;
